ac2061bc5b6290b5666025a52405f6f51bf3d559
Darrion Gering and Mathew Akre <darrion@>
2019-04-02 09:56:15 -0700

    add if statement so no input and negative numbers will not run.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   $("#theForm").submit(function(event) {
       var numberTo = parseInt($("#numberTo").val());
       var numberBy = parseInt($("#numberBy").val());
-      countBy(numberTo, numberBy);
+      if (isNaN(numberTo) || isNaN(numberBy)) {
+        console.log("Not a Number");
+      } else if (numberTo < 0 || numberBy < 0){
+        console.log("negative number")
+      } else {
+        countBy(numberTo, numberBy);
+      }
       event.preventDefault();
   });
 });
+
 
 var countBy = function(countTo, countBy) {
   for(var sum = countBy; sum <= countTo; sum+=countBy) {
